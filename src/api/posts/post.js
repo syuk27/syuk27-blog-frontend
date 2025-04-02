915d7d6b2b403e9b6d29af9943de64957cb75aba
lilaye <lilaye@naver.com>
2025-04-02 23:04:19 +0900

    2025-04-02 test

--- src/api/posts/post.js
+++ src/api/posts/post.js
@@ -1,29 +1,36 @@
 import { apiClient } from "../common";
 
 const createAdminPost = (post) => {
-    return apiClient.post("/admin/posts", post);
-}
+  return apiClient.post("/admin/posts", post);
+};
 
 const getAdminPosts = (page) => {
-    return apiClient.get(`/admin/posts/${page}`);
-}
+  return apiClient.get(`/admin/posts/${page}`);
+};
 
 const getAdminPostById = (postId) => {
-    return apiClient.get(`/admin/posts/detail/${postId}`);
-}
+  return apiClient.get(`/admin/posts/detail/${postId}`);
+};
 
 const createUserPost = (post) => {
-    return apiClient.post("/user/posts", post);
-}
+  return apiClient.post("/user/posts", post);
+};
 
 const getUserPosts = (id, page) => {
-    return apiClient.get(`/user/posts/${id}/${page}`);
-}
+  return apiClient.get(`/user/posts/${id}/${page}`);
+};
+
+const createAdminPostImage = (formData) => {
+  return apiClient.post("/admin/posts/v2/image", formData, {
+    Headers: { "Content-Type": "multipart/form-data" },
+  });
+};
 
 export {
-    createAdminPost,
-    getAdminPosts,
-    getAdminPostById,
-    createUserPost,
-    getUserPosts,
-}
+  createAdminPost,
+  getAdminPosts,
+  getAdminPostById,
+  createUserPost,
+  getUserPosts,
+  createAdminPostImage,
+};
